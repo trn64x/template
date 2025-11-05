5e84a038ac4231666f386ab3a40d01b2d6e99942
Maciej Potrz <maciekp371@gmail.com>
2025-11-06 00:21:42 +0100

    Update/Product-list

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,16 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import ProductCard from "./product-card";
+import { Product } from "@/types";
 const ProductList = ({data,title,limit}:{data:any, title?: string,limit?: number}) => {
     const limitedData = limit ? data.slice(0,limit): data;
     return ( <div className="my-10">
         <h2 className="h2-bold mb-4">{title}</h2>
         {data.length > 0 ? (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {limitedData.map((product:any)=> {
-                    return(
-                    <ProductCard product={product} key={product.slug}>{product.name}</ProductCard>
-                )
-                })}
+                {limitedData.map((product:Product)=> (
+                    <ProductCard product={product} key={product.slug}/>
+                ))}
             </div>
         ):(<div><p>No Products Found</p></div>)}
     </div> );
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,14 +1,19 @@
 'use server';
-import { PrismaClient } from "../generated/prisma/client";
+import {prisma} from "../../db/prisma"
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constans";
 // Get latest products
 export async function getLatestProducts(){
-    const prisma = new PrismaClient();
-
     const data = await prisma.product.findMany({
         take: LATEST_PRODUCTS_LIMIT,
         orderBy:{createdAt: 'desc'}
     });
     return convertToPlainObject(data);
+}
+
+// Get single product by it's slug
+export async function getProductBySlug(slug:string){
+    return await prisma.product.findFirst({
+        where:{ slug: slug},
+    });
 }
